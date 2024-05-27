78baae9995b587e6340cbcf4a37bb6e8b4bb46c8
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-05-27 23:04:53 +0700

    main index and js

--- src/js/weatherApp.js
+++ src/js/weatherApp.js
@@ -1,9 +1,15 @@
-
 import { handleError } from "./handleError.js";
 import { currentWeatherData } from "./currentWeatherData.js";
+import { weatherForecastData } from "./weatherForecastData.js";
 import callApi from "./callApi.js";
+import {endLoadingState, startLoadingState} from "./setLoadingState.js";
+import {createDailyCards, createHourlyCards} from "./weatherForecastCards.js";
 
 const searchBoxInput = document.querySelector(".search-box-input");
+
+createHourlyCards();
+createDailyCards();
+
 // 3.Xu ly du lieu
 searchBoxInput.addEventListener("keyup", async (event) => {
     if (event.keyCode === 13) { // Code of enter
@@ -22,13 +28,78 @@ searchBoxInput.addEventListener("keyup", async (event) => {
             const response = await callApi(searchBoxInput.value);
             //6 Gui du lieu
             await currentWeatherData(response)
+
+            try {
+                await startLoadingState();
+                await weatherForecastData(searchBoxInput.value);
+                await endLoadingState();
+            } catch (error) {
+                if (error.message === "Failed to fetch") {
+                    await handleError(
+                        "Uh oh! It looks like you're not connected to the internet. Please check your connection and try again.",
+                        "Refresh Page"
+                    );
+                } else {
+                    await handleError(error.message, "Try Again");
+                }
+            }
         }
     }
 });
+
+//lay vi tri hien tai cua nguoi dung
+const gpsButton = document.querySelector(".gps-button");
+const getUserLocation = async () => {
+    const successCallback = async (position) => {
+        const data = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+        };
+        const response = await callApi(data);
+        //6 Gui du lieu
+        await currentWeatherData(response)
+        try {
+            await startLoadingState();
+            await weatherForecastData(data);
+            await endLoadingState();
+        } catch (error) {
+            if (error.message === "Failed to fetch") {
+                await handleError(
+                    "Uh oh! It looks like you're not connected to the internet. Please check your connection and try again.",
+                    "Refresh Page"
+                );
+            } else {
+                await handleError(error.message, "Try Again");
+            }
+        }
+    };
+
+    const errorCallback = (error) => {
+        console.log(error);
+        fetchWeatherData("Istanbul");
+    };
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+};
+gpsButton.addEventListener("click", getUserLocation);
+// getUserLocation();
+
+
+const topButton = document.querySelector(".top-button");
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
+topButton.addEventListener("click", scrollToTop);
+
+
 const checkValue = (valueInput) => {
     if(valueInput.length <= 0) {
         return false;
     }
     return true;
 }
+
 
