ee978d7b8af6cee94e44160b3d8df56d5ce71755
dzay <uyenphuong6886@gmail.com>
2024-05-28 20:29:36 +0700

    button

---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+// Tro ly ao
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const synth = window.speechSynthesis;
+recognition.lang = 'vi-VI';
+recognition.continuous = false;
+
+const microphone = document.querySelector('.microphone-button');
+
+const speak = (text) => {
+    if (synth.speaking) {
+        console.error('Busy. Speaking...');
+        return;
+    }
+
+    const utter = new SpeechSynthesisUtterance(text);
+
+    utter.onend = () => {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utter.onerror = (err) => {
+        console.error('SpeechSynthesisUtterance.onerror', err);
+    }
+
+    synth.speak(utter);
+};
+
+const handleVoice = (text) => {
+    console.log('text', text);
+
+    // "thời tiết tại Đà Nẵng" => ["thời tiết tại", "Đà Nẵng"]
+    const handledText = text.toLowerCase();
+    if (handledText.includes('thời tiết tại')) {
+        const location = handledText.split('tại')[1].trim();
+
+        console.log('location', location);
+        searchInput.value = location;
+        const changeEvent = new Event('change');
+        searchInput.dispatchEvent(changeEvent);
+        return;
+    }
+
+    const container = document.querySelector('.container');
+    if (handledText.includes('thay đổi màu nền')) {
+        const color = handledText.split('màu nền')[1].trim();
+        container.style.background = color;
+        return;
+    }
+
+    if (handledText.includes('màu nền mặc định')) {
+        container.style.background = '';
+        return;
+    }
+
+    if (handledText.includes('mấy giờ')) {
+        const textToSpeech = `${moment().hours()} hours ${moment().minutes()} minutes`;
+        speak(textToSpeech);
+        return;
+    }
+
+    speak('Try again');
+}
+
+microphone.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    recognition.start();
+    microphone.classList.add('recording');
+});
+
+recognition.onspeechend = () => {
+    recognition.stop();
+    microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+    console.error(err);
+    microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
+}
---
+++ src/js/weatherApp.js
@@ -0,0 +1,90 @@
+import { createHourlyCards, createDailyCards } from "./weatherForecastCards.js";
+import { startLoadingState, endLoadingState } from "./setLoadingState.js";
+import { handleError } from "./handleError.js";
+import { currentWeatherData } from "./currentWeatherData.js";
+import { weatherForecastData } from "./weatherForecastData.js";
+
+
+
+const searchBoxInput = document.querySelector(".search-box-input");
+const gpsButton = document.querySelector(".gps-button");
+const topButton = document.querySelector(".top-button");
+
+createHourlyCards();
+createDailyCards();
+const API_KEY = "66251d0370e6ab0032d63bffb24c2d95";
+
+const fetchWeatherData = async (data) => {
+  try {
+    await startLoadingState();
+    await currentWeatherData(data);
+      await weatherForecastData(data, API_KEY);
+    await endLoadingState();
+  } catch (error) {
+    if (error.message === "Failed to fetch") {
+      await handleError(
+        "Uh oh! It looks like you're not connected to the internet. Please check your connection and try again.",
+        "Refresh Page"
+      );
+    } else {
+      await handleError(error.message, "Try Again");
+    }
+  }
+};
+
+const getUserLocation = async () => {
+  const successCallback = async (position) => {
+    const data = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    };
+
+    await fetchWeatherData(data);
+  };
+
+  const errorCallback = (error) => {
+    console.log(error);
+    fetchWeatherData("Istanbul");
+  };
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+//2.An enter
+searchBoxInput.addEventListener("keyup", async (event) => {
+  if (event.keyCode === 13) { // Code of enter
+    if(!checkValue(searchBoxInput.value)) {
+      //4.1 bao loi yeu cau nguoi dung nhap lai
+      handleError(
+          "Vui lòng nhập tên thành phố cần tìm kiếm!.",
+          "Refresh Page"
+      )
+    }else {
+      //5 ket noi xuong api
+      await fetchWeatherData(searchBoxInput.value);
+    }
+
+  }
+});
+//4.Kiem tra du lieu
+const checkValue = (valueInput) => {
+  if(valueInput.length <= 0) {
+    return false;
+  }
+  return true;
+}
+gpsButton.addEventListener("click", getUserLocation);
+
+// ctaButton.addEventListener("click", () => {
+//   window.open("https://github.com/pekkiriscim/weather");
+// });
+
+topButton.addEventListener("click", scrollToTop);
+
+getUserLocation();
